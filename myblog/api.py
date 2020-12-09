d8881ad28acce4da8a70e663eef939606e6a2c6f
charliechensz <charlie.chen.1688@hotmail.com>
2020-12-09 13:14:36 +0800

    20-1209

--- myblog/api.py
+++ myblog/api.py
@@ -1,11 +1,4 @@
-'''
-Author: your name
-Date: 2020-11-26 16:03:43
-LastEditTime: 2020-11-26 22:24:21
-LastEditors: Please set LastEditors
-Description: In User Settings Edit
-FilePath: /myblog/myblog/api.py
-'''
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from myblog.models import Classes, Userinfo
@@ -35,20 +28,3 @@ def api_test(request):
         data['classes'].append(data_item)
     return Response(data)
 
-    # classes = Classes.objects.all() 
-    # classes_data = Classes_data(classes,many=True) 
-    # userinfo = Userinfo.objects.all() 
-    # #print(userinfo)
-    # userinfo_data = Userinfo_data(userinfo,many=True) 
-    # #print(userinfo_data)
-    # print(userinfo_data.data)
-    
-    # data = {
-    #     "classes": classes_data.data,
-    #     "userinfo": userinfo_data.data
-    # }
-    #return Response({'data':data})
-    
-
-   
-    # verison 2.1 
